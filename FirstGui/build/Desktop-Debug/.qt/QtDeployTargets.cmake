85d2a8bc41c7e64415c59cd5d410db6544653a9e
Sandeep Reddy <modugusandy@gmail.com>
2024-12-16 17:51:45 +0100

    commit

---
+++ FirstGui/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_FirstGui_FILE C:/Users/modug/OneDrive/Desktop/QT5 Creator/FirstGui/build/Desktop-Debug/FirstGui.exe)
+set(__QT_DEPLOY_TARGET_FirstGui_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_FirstGui_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
